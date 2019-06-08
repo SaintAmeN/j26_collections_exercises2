c8f22c260bd28d74c0b223b0aa5d6c9fbc619ede
Paweł Recław <pawel.reclaw@gmail.com>
2019-06-08 15:52:25 +0200

    autostrada

--- src/main/java/com/javagda26/zad2_autostrada/Main.java
+++ src/main/java/com/javagda26/zad2_autostrada/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Autostrada autostrada = new Autostrada();
 
         // zmienna w której będziemy przechowywać komendę wpisaną przez użytkownika
         String komenda;
@@ -22,21 +23,34 @@ public class Main {
                 // poza słowem "wjazd" komenda wymaga 2 parametrów, więc długość tablicy to minimum 3
                 if (słowa.length >= 3) {
                     String nrRej = słowa[1];
-                    String typPojazdu = słowa[2];
+                    String slowoTypPojazdu = słowa[2];
 
+                    // jeśli posiadacie string który reprezentuje jakiegoś enuma
+                    // np. OSOBOWY - enum OSOBOWY
+                    // każdy enum ma metodę valueof - która zmienia string na enum
+                    try {
+                        TypPojazdu typ = TypPojazdu.valueOf(slowoTypPojazdu.toUpperCase());
 
+                        autostrada.wjazdPojazduNaAutostradę(nrRej, typ);
+                    }catch (IllegalArgumentException iae){
+                        // wyjątek rzuci się jeśli zamiast np. Osobowy zrobię literówkę lub wpiszę słowo którego enuma nie mamy
+                        // np. hulajnoga
+                        System.err.println("Błąd, zły typ pojazdu.");
+                    }
                 }
             } else if (słowa[0].equalsIgnoreCase("wyjazd")) {
                 // poza słowem "wyjazd" komenda wymaga 1 parametru, więc długość tablicy to minimum 2
                 if (słowa.length >= 2) {
                     String nrRej = słowa[1];
 
+                    System.out.println(autostrada.wyjazdPojazdu(nrRej));
                 }
             } else if (słowa[0].equalsIgnoreCase("sprawdz")) {
                 // poza słowem "sprawdz" komenda wymaga 1 parametru, więc długość tablicy to minimum 2
                 if (słowa.length >= 2) {
                     String nrRej = słowa[1];
 
+                    System.out.println(autostrada.znajdzPojazd(nrRej));
                 }
             }
 
